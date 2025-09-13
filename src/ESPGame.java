e8a2d7a3c1b7f1ffaf1e2def9fd319d84f1ab0a8
thien <143459191+workaccount3@users.noreply.github.com>
2025-09-13 18:21:57 -0400

    added logic for color options, planning on choosing random color  next

--- src/ESPGame.java
+++ src/ESPGame.java
@@ -1,36 +1,103 @@
 import java.util.Scanner;
-import java.io.File;
 import java.io.IOException;
-import java.util.Random;
+import java.io.File;
 
 public class ESPGame{
-	public static void main(String[] args)throws IOException{
+	public static void main(String[] args)throws IOException {
+	final String fileName = "colors.txt";
+	Boolean isCorrectInput = false;
 	
 	Scanner key = new Scanner(System.in);
-	Scanner inputFile = new Scanner(new File("colors.txt"));
-	String userColor = "";
-	Random rn = new Random();	
-	int options = 0;	
+	String colors[];
+	colors = new String[16];
+	int upperLimit = 0;
 
 	System.out.println("a. 16 colors");
 	System.out.println("b. 10 colors ");
 	System.out.println("c. 5 colors");
 	System.out.println("d. exit");
 	char userInput = key.nextLine().charAt(0);
-	
-	switch(userInput){
-		case 'a' ->{
-			while(inputFile.hasNextLine()){
-				String color =  inputFile.nextLine();
-				System.out.println(color);
-				colors = rn.nextInt(1,16);
+	int userNum;
+
+
+	try(Scanner fileRead = new Scanner(new File(fileName))){
+		int count = 0;
+
+		while(fileRead.hasNext()){
+			String color =  fileRead.nextLine();
+			colors[count] = color;
+			count++;
+		}
+
+		while(isCorrectInput == false){
+			switch(userInput){
+				case 'a' ->{
+					for(int i =0; i<16; i++){
+						System.out.println("Color #" + (i+1) + ": " + colors[i]);
+					}	
+					isCorrectInput = true;
+					upperLimit = 16;
+				}
+
+				case 'b' ->{
+					for(int i =0; i<10; i++){
+						System.out.println("Color #" + (i+1) + ": " + colors[i]);
+					}	
+					isCorrectInput = true;
+					upperLimit = 10;
+				}
+
+				case 'c' ->{
+					for(int i =0; i<5; i++){
+						System.out.println("Color #" + (i+1) + ": " + colors[i]);
+					}	
+					isCorrectInput = true;
+					upperLimit =5;
+				}
+
+				case 'd' ->{
+                    isCorrectInput = true;	
+					System.exit(0);
+				}
+
+				default ->{
+					System.out.println("Invalid Input. Try again.");
+					isCorrectInput = false;
+					userInput = key.nextLine().charAt(0);
+				}
 			}
 		}
-	}
+}
+
+int randomNum;
+
+for(int i =0; i< 3; i++){
+	randomNum = Math.random(upperLimit);
+	System.out.println(colors[randomNum]);
+
+	System.out.println("Choose a color number...");
+	userNum = key.nextInt();
+
+		if(userNum == randomNum){
+	System.out.println(colors[randomNum]);
+
+			System.out.println("win!");
+		}else{
+
+	System.out.println(colors[randomNum]);
+			System.out.println("Fail");
+		}
+
+}
+
+	
 
 
-	inputFile.close();
-		System.out.println("End of program");
+
+	key.close();
+	
+// fileRead auto closes, no need to close unless for documentation:
+	System.out.println("End of program");
 
 	}
 }
