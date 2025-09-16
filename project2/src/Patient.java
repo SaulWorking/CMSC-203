1817fcddbb0f8fdc2a8d75d0f49c9f0a067f886d
workaccount3 <143459191+workaccount3@users.noreply.github.com>
2025-09-16 11:56:34 -0400

    project 2 start

---
+++ project2/src/Patient.java
@@ -0,0 +1,26 @@
+
+
+public class Patient{
+	public:
+	String firstName, middleName, lastName;
+	String address, city, state, ZIP;
+	String phoneNum;
+	String emergencyContact, emergencyPhoneNum;
+
+	Patient(){
+
+	}
+
+	Patient(String firstName, String middleName, String lastName){
+	this.firstName = firstName;
+	this.firstName = firstName;
+	this.firstName = firstName;
+	}
+	
+	Patient(){
+	
+		
+	}
+	private:
+
+}
