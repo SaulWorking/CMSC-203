7a4303847d34c3cd8401acf13011327a76bf3636
thien <143459191+workaccount3@users.noreply.github.com>
2025-09-25 22:11:08 -0400

    created getter and setter functions

--- project2/src/Patient.java
+++ project2/src/Patient.java
@@ -1,26 +1,75 @@
-
-
 public class Patient{
-	public:
-	String firstName, middleName, lastName;
-	String address, city, state, ZIP;
-	String phoneNum;
-	String emergencyContact, emergencyPhoneNum;
+	
+		private String firstName, middleName, lastName;
+		private String address, city, state, ZIP;
+		private	String phoneNum;
+		private	String emergencyContact, emergencyPhoneNum;
 
 	Patient(){
-
+		this.firstName = "";
+		this.middleName = "";
+		this.lastName = "";	
+		this.address = "";
+		this.city = "";
+		this.state = "";
+		this.ZIP = "";
+		this.emergencyContact = "";
+		this.emergencyPhoneNum = "";
 	}
 
 	Patient(String firstName, String middleName, String lastName){
-	this.firstName = firstName;
-	this.firstName = firstName;
-	this.firstName = firstName;
-	}
+		this.firstName = firstName;
+		this.firstName = firstName;
+		this.firstName = firstName;
+	}  
 	
-	Patient(){
-	
-		
+	Patient(String firstName, String middleName, String lastName, String address, String city, String state, String ZIP, String phoneNum, String emergencyContact, String emergencyPhoneNum){
+		this(firstName, middleName, lastName);	
+
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.ZIP = ZIP;
+		this.emergencyContact = emergencyContact;
+		this.emergencyPhoneNum = emergencyPhoneNum;
 	}
-	private:
+
+    public String getFirstName() { return firstName; }
+    public String getMiddleName() { return middleName; }
+    public String getLastName() { return lastName; }
+    public String getAddress() { return address; }
+    public String getCity() { return city; }
+    public String getState() { return state; }
+    public String getZIP() { return ZIP; }
+    public String getPhoneNum() { return phoneNum; }
+    public String getEmergencyContact() { return emergencyContact; }
+    public String getEmergencyPhoneNum() { return emergencyPhoneNum; }
+
+ 	public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setMiddleName(String middleName) { this.middleName = middleName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setAddress(String address) { this.address = address; }
+    public void setCity(String city) { this.city = city; }
+    public void setState(String state) { this.state = state; }
+    public void setZIP(String ZIP) { this.ZIP = ZIP; }
+    public void setPhoneNum(String phoneNum) { this.phoneNum = phoneNum; }
+    public void setEmergencyContact(String emergencyContact) { this.emergencyContact = emergencyContact; }
+    public void setEmergencyPhoneNum(String emergencyPhoneNum) { this.emergencyPhoneNum = emergencyPhoneNum;
+	 }
+	 
+	@Override
+	public String toString() {
+    return "Patient Information:\n" +
+           "First Name: " + getFirstName() + "\n" +
+           "Middle Name: " + getMiddleName() + "\n" +
+           "Last Name: " + getLastName() + "\n" +
+           "Address: " + getAddress() + "\n" +
+           "City: " + getCity() + "\n" +
+           "State: " + getState() + "\n" +
+           "ZIP: " + getZIP() + "\n" +
+           "Phone Number: " + getPhoneNum() + "\n" +
+           "Emergency Contact: " + getEmergencyContact() + "\n" +
+           "Emergency Phone Number: " + getEmergencyPhoneNum();
+}
 
 }
--- project2/src/Procedure.java
+++ project2/src/Procedure.java
@@ -1,1 +1,46 @@
+public class Procedure {
+    private String procedureName;
+    private String date;
+    private double charge;
+    private String practitionerName;
 
+    public Procedure() {
+        this.procedureName = "";
+        this.date = "";
+        this.charge = 0.0;
+        this.practitionerName = "";
+    }
+
+    public Procedure(String procedureName, String date) {
+        this.procedureName = procedureName;
+        this.date = date;
+        this.charge = 0.0;
+        this.practitionerName = "";
+    }
+
+    public Procedure(String procedureName, String date, double charge, String practitionerName) {
+        this.procedureName = procedureName;
+        this.date = date;
+        this.charge = charge;
+        this.practitionerName = practitionerName;
+    }
+
+    public String getProcedureName() { return procedureName; }
+    public String getDate() { return date; }
+    public double getCharge() { return charge; }
+    public String getPractitionerName() { return practitionerName; }
+
+    public void setProcedureName(String procedureName) { this.procedureName = procedureName; }
+    public void setDate(String date) { this.date = date; }
+    public void setCharge(double charge) { this.charge = charge; }
+    public void setPractitionerName(String practitionerName) { this.practitionerName = practitionerName; }
+
+	@Override
+	public String toString() {
+        return "Procedure Information:\n" +
+               "Procedure Name: " + getProcedureName() + "\n" +
+               "Date: " + getDate() + "\n" +
+               "Charge: $" + getCharge() + "\n" +
+               "Practitioner: " + getPractitionerName();
+    }
+}
