60f6b116ec78ed93cc8614eb9282dd276bb446bc
workaccount3 <143459191+workaccount3@users.noreply.github.com>
2025-10-24 13:33:32 -0400

    fixed

---
+++ project4/src/Property.java
@@ -0,0 +1,61 @@
+public class Property {
+
+    private String city;
+    private String owner;
+    private Plot plot;
+    private String propertyName;
+    private double rentAmount;
+
+    public Property() {
+        propertyName = "";
+        city = "";
+        owner = "";
+        rentAmount = 0;
+        plot = new Plot();
+    }
+
+    public Property(String propertyName, String city, double rentAmount, String owner) {
+        this.propertyName = propertyName;
+        this.city = city;
+        this.rentAmount = rentAmount;
+        this.owner = owner;
+        this.plot = new Plot();
+    }
+
+    public Property(String propertyName, String city, double rentAmount, String owner, int x, int y, int width, int depth) {
+        this.propertyName = propertyName;
+        this.city = city;
+        this.rentAmount = rentAmount;
+        this.owner = owner;
+        this.plot = new Plot(x, y, width, depth);
+    }
+
+    public Property(Property otherProperty) {
+        this.propertyName = otherProperty.propertyName;
+        this.city = otherProperty.city;
+        this.rentAmount = otherProperty.rentAmount;
+        this.owner = otherProperty.owner;
+        this.plot = new Plot(otherProperty.plot);
+    }
+
+
+    public String getCity() {return city;}
+    public String getOwner() {return owner;}
+    public Plot getPlot() {return plot;}
+    public String getPropertyName() {return propertyName;}
+    public double getRentAmount() {return rentAmount;}
+
+    public void setCity(String city) {this.city = city;}
+    public void setOwner(String owner) {this.owner = owner;}
+    public void setPlot(Plot plot) {this.plot = plot;}
+    public void setPropertyName(String propertyName) {this.propertyName = propertyName;}
+    public void setRentAmount(double rentAmount) {this.rentAmount = rentAmount;}
+
+    public String toString(){
+        String propertyInfo = propertyName + "," + 
+                              city + "," + 
+                              owner + "," + 
+                              rentAmount;
+        return propertyInfo;
+    }
+}
