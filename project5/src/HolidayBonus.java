d5946c6a1b0018ae3b8affd526b8f79c8ab5f3e1
workaccount3 <143459191+workaccount3@users.noreply.github.com>
2025-11-05 10:33:52 -0500

    added skeletons for utility classes

---
+++ project5/src/HolidayBonus.java
@@ -0,0 +1,17 @@
+public class HolidayBonus{
+
+
+	public static double[] calculateHolidayBonus(double[][] data){
+		double[] array;
+		
+		return array;	
+	}
+
+
+	public static double calculuateTotalHolidayBonus(double[][] data){
+		double holidayBonus = 0.0;
+
+		reutrn holidayBonus;	
+	
+	}
+}
