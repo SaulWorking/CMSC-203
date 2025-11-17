e2f7eb1509c19a51f1c22431ce094127ce906e28
thien <143459191+SaulWorking@users.noreply.github.com>
2025-11-17 16:58:03 -0500

    added functionality for utility class and holidaybonus

--- project5/src/TwoDimRaggedArrayUtility.java
+++ project5/src/TwoDimRaggedArrayUtility.java
@@ -1,56 +1,99 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.FileNotFoundException;
 
-	public final class TwoDimRaggedArrayUtilty {
+	public final class TwoDimRaggedArrayUtility {
 
 	private static final int MAX_ROW = 10;
 	private static final int MAX_COL = 10;
 
-	public static double[][] readFile(File file) throws FileNotFoundException {
-		double [][] arrayInfo = new double[MAX_ROW][];
+public static double[][] readFile(File file) throws FileNotFoundException {
+    String[][] fileInfo = new String[MAX_ROW][MAX_COL];
 
-		try(Scanner fileReader = new Scanner(file)){
-			int rowNum = 0;
-			
-			while(fileReader.hasNextLine()){
-				String costs = fileReader.nextLine();
-				String[] data = costs.split(" ");
+	//check for number of rows
+    int rowNum = 0;
 
-				for(int col=0; col<data.length; col++){
-					double val = Double.parseDouble(data[col]);
-					arrayInfo[rowNum][col] = val;
-					System.out.println(arrayInfo[rowNum][col]);
-									
-				}
+	//set up fileInfo in preparation for conversion to double array
+    try (Scanner fileReader = new Scanner(file)) {
+        while (fileReader.hasNextLine()) {
 
-				rowNum++;
-			}		
+            String line = fileReader.nextLine();
+            String[] data = line.split(" ");
+
+            for (int col = 0; col < data.length; col++) {
+                fileInfo[rowNum][col] = data[col];
+            }
+
+            rowNum++;
+        }
+    }
+
+    double[][] parkData = new double[rowNum][];
+
+	//for each row, determine length of that row (number of columns per row)
+    for (int i = 0; i < rowNum; i++) {
+
+        int currentLength = 0;
+        while (currentLength < MAX_COL){
+
+			if(fileInfo[i][currentLength] != null) {
+				currentLength++;
+			} else {
+				break;
+			}
 		}
 
-		return arrayInfo;
-	}
+		//create jagged array based on current row length
+        parkData[i] = new double[currentLength];
+
+		//fill jagged array with double values
+        for (int j = 0; j < currentLength; j++) {
+            parkData[i][j] = Double.parseDouble(fileInfo[i][j]);
+        }
+    }
+
+    return parkData;
+}
+
+public static void writeToFile(double[][] data, File outputFile) throws FileNotFoundException {
+
 	
+    try (PrintWriter fileWrite = new PrintWriter(outputFile)) {
 
-	public static void writeToFile(double[][] data, File outputFile) throws FileNotFoundException{
+		//for every row in data, create a string of the row's value seperated by spaces
+        for (int row = 0; row < data.length; row++) {
 
-		//for however many rows,
+            if (data[row].length == 0) {
+                fileWrite.print('\n');
+                continue;
+            }
 
-		//read one row of array
-		//write to file for one line
-		//
-		//do this
-		
-	
-	}
+            StringBuilder costRow = new StringBuilder();
+
+            for (int col = 0; col < data[row].length; col++) {
+				//add value to line
+                costRow.append(data[row][col]);
+
+				//check if not last value to avoid space at end of line
+                if (col < data[row].length - 1) {
+                    costRow.append(" ");
+                }
+            }
+			//write line to file
+            fileWrite.println(costRow.toString());
+        }
+
+    }
+}
 
 	public static double getTotal(double[][] data){
 		double sum = 0.0;
 
 		for(int row=0; row<data.length;row++){
 
-			for(int col =0 ; col<data[row].length; col++){
+			for(int col =0; col<data[row].length; col++){
 				sum+= data[row][col];
 			}	
 		}
@@ -60,84 +103,143 @@ import java.io.FileNotFoundException;
 
 	public static double getAverage(double[][] data){
 		double sum = 0;
+		int count  =0;
 
-		return sum;
+		for(int row=0; row<data.length;row++){
+			for(int col =0; col<data[row].length; col++){
+					sum+= data[row][col];
+					count++;
+			}	
+		}
+		double average = sum/count;
+		return average;
 	}
 
 	public static double getRowTotal(double[][] data, int row){
 		double sum =0;	
 		
 		for(int col = 0; col<data[row].length;col++){
-		
 			sum += data[row][col];
-			
-		}	
-
+		}
 		return sum;
 	
 	}
 
 	public static double getColumnTotal(double[][] data, int col){
-		double sum =0 ;
+		double sum =0;
 
-		for(int row =0; row<data[col].length; row++){
-		
-			sum += data[row][col];
-		
-		}	
+		for(int row = 0; row<data.length; row++){
+			if(col < data[row].length){ 
+				sum += data[row][col];
+			}	
+		}
 	
 		return sum;
 	}
 
 	public static double getHighestInRow(double[][] data, int row){
 		double highest = 0;
+		for(int col = 0; col < data[row].length; col++){
+			if(highest < data[row][col]){
+				highest = data[row][col];
+			}
+		}
+
 		return highest;	
 	}
 
-	public static int getHighestInRow(double[][] data, int row){
+	public static int getHighestInRowIndex(double[][] data, int row){
 		int index = 0;
+		for(int col = 0; col < data[row].length; col++){
+			if(data[row][index] < data[row][col]){
+				index = col;
+			}
+		}
+
 		return index;	
 	}
 
 	public static double getLowestInRow(double[][] data, int row){
 		double lowest = 0;
+		for(int col = 0; col < data[row].length; col++){
+			if(lowest > data[row][col]){
+				lowest = data[row][col];
+			}
+		}
 		return lowest;	
 	}
 
 	public static int getLowestInRowIndex(double[][] data, int row){
 		int index = 0;
+		for(int col = 0; col < data[row].length; col++){
+			if(data[row][index] > data[row][col]){
+				index = col;
+			}
+		}
 		return index;	
 	}
 
 	public static double getHighestInColumn(double[][] data, int col){
-		double highest = 0;
+		double highest = Double.MIN_VALUE;
+		for(int row = 0; row < data.length; row++){
+			if(col < data[row].length && highest < data[row][col]){
+				highest = data[row][col];
+			}
+		}
 		return highest;	
 	}
 
-	public static double getHighestInColumnIndex(double[][] data, int col){
-		double index = 0;
+	public static int getHighestInColumnIndex(double[][] data, int col){
+		int index = 0;
+		for(int row = 0; row < data.length; row++){
+			if(col < data[row].length && data[index][col] < data[row][col]){
+				index = row;
+			}
+		}
 		return index;	
 	}
 
-	public static double getLowestInColumn(double[][] data, int row){
-		double lowest = 0;
+	public static double getLowestInColumn(double[][] data, int col){
+		double lowest = Double.MAX_VALUE;
+		for(int row = 0; row < data.length; row++){
+			if(col < data[row].length && lowest > data[row][col]){
+				lowest = data[row][col];
+			}
+		}
 		return lowest;	
 	}
 
-	public static int getLowestInColumnIndex(double[][] data, int row){
+	public static int getLowestInColumnIndex(double[][] data, int col){
 		int index = 0;
+		for(int row = 0; row < data.length; row++){
+			if(data[row].length > col && data[row][col] < data[index][col]){
+				index = row;
+			}
+		}
 		return index;	
 	}
 
 	public static double getHighestInArray(double[][] data){
-		double highest = 0;
+		double highest = Double.MIN_VALUE;
+		for(int row = 0; row < data.length; row++){
+			for(int col = 0; col < data[row].length; col++){
+				if(highest < data[row][col]){
+					highest = data[row][col];
+				}
+			}
+		}
 		return highest;
 	}
 
 	public static double getLowestInArray(double[][] data){
-		double lowest = 0;
+		double lowest = Double.MAX_VALUE;
+		for(int row = 0; row < data.length; row++){
+			for(int col = 0; col < data[row].length; col++){
+				if(lowest > data[row][col]){	
+					lowest = data[row][col];
+				}
+			}
+		}
 		return lowest;	
 	}
-
-
 }
