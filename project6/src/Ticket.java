d80b52f9663bdf8d2fc8a4277fa8df337996962a
thien <143459191+SaulWorking@users.noreply.github.com>
2025-11-18 20:16:06 -0500

    added

--- project6/src/Ticket.java
+++ project6/src/Ticket.java
@@ -11,6 +11,25 @@ public class Ticket {
 
     // Calculate ticket price based on day of week and age
 
+    public double calculatePrice() {
+        double finalPrice = basePrice;
+        if(dayOfWeek.equals("saturday") || dayOfWeek.equals("sunday"){
+
+            return finalPrice; 
+        }else{
+            //the regular discount
+            finalPrice = finalPrice * (1 - MONDAY.discount);
+        }
+
+        if(age > 100){
+
+            finalPrice *= 1000;
+
+        }
+
+
+    }
+
         // Apply discounts for weekdays
         // Apply age-based discounts
 
